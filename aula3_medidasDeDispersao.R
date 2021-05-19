b8fb454112e2f4ebcb8f45cf5d9f48c3bf367bd7
Nicole Brum <nicolembrum@gmail.com>
2021-05-19 01:17:43 +0000

    variância e desvio padrão

--- aula3_medidasDeDispersao.R
+++ aula3_medidasDeDispersao.R
@@ -1,14 +1,31 @@
 ## HISTOGRAMA
-x <- c(159, 200, 201, 174, 175, 200, 164, 164, 160, 160, 170, 170, 201, 170, 178, 178, 200)
+x <- c(159, 161, 162, 164, 166, 167, 168, 168, 169,170, 171, 171, 172, 173, 173, 174, 174, 174, 175,176, 177, 178, 179, 180, 181, 183, 185, 190, 194,201)
 grafico1 <- hist(x, 
-                 main="Histograma", 
-                 xlab="cm", 
-                 ylab="freq", 
-                 border="blue", col="green",
-                 las=2,
-                 breaks=5)
+                 main="Histograma da Altura",
+                 xlab="cm",
+                 ylab="frequência",
+                 border="blue",
+                 col="green",
+                 xlim=c(150,210),
+                 las=1,breaks=5,
+                 right=FALSE)
 
-mean(x)
+
+## VARIÂNCIA (AMOSTRAL)
+varX <- var(x)
+varX
+
+
+
+## DESVIO PADRÃO
+sdX <- sd(x)
+sdX
+
+
+## COVARIÂNCIA
+
+
+
 
 
 
